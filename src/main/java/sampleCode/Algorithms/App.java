befe2e0c4523d4418320f5ec730c6e917a60ecc7
saurav <sauravpd58@gmail.com>
2018-07-27 00:46:14 +0530

    Initial commit

---
+++ src/main/java/sampleCode/Algorithms/App.java
@@ -0,0 +1,13 @@
+package sampleCode.Algorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
