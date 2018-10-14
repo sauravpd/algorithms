9bd3c18c2e3d7c3b86b94740aac06ee74486d352
saurav <sauravpd58@gmail.com>
2018-10-15 00:31:53 +0530

    commit changes in BubbleSortExample

--- src/main/java/sampleCode/sorting/bubblesort/BubbleSortExample.java
+++ src/main/java/sampleCode/sorting/bubblesort/BubbleSortExample.java
@@ -1,24 +1,38 @@
 package sampleCode.sorting.bubblesort;
 
+/**
+ * BubbleSortExample
+ * @author saurav
+ *
+ */
 public class BubbleSortExample 
 {
 	public static void main(String[] args)
 	{
 		int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;
-				lastUnsortedIndex--) {
-			for (int i = 0; i < lastUnsortedIndex; i++) {
-				if (intArray[i] > intArray[i + 1]) {
+		for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0;lastUnsortedIndex--) 
+		{
+			for (int i = 0; i < lastUnsortedIndex; i++) 
+			{
+				if (intArray[i] > intArray[i + 1]) 
+				{
 					swap(intArray, i, i + 1);
 				}
 			}
 		}
-		for (int i = 0; i < intArray.length; i++) {
+		for (int i = 0; i < intArray.length; i++) 
+		{
 			System.out.println(intArray[i]);
 		}
 	}
 
+	/**
+	 * swap the number
+	 * @param array
+	 * @param i
+	 * @param j
+	 */
 	public static void swap(int[] array, int i, int j) 
 	{
 		if (i == j) 
