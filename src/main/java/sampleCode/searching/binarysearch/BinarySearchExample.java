5dc327352fecf12b2086f9c9b6d19e530d4e1265
saurav <sauravpd58@gmail.com>
2018-10-17 01:09:22 +0530

    commit chnages in code format

--- src/main/java/sampleCode/searching/binarysearch/BinarySearchExample.java
+++ src/main/java/sampleCode/searching/binarysearch/BinarySearchExample.java
@@ -22,46 +22,54 @@ public class BinarySearchExample
 		System.out.println(recursiveBinarySearch(intArray, 1));
 	}
 
-	public static int iterativeBinarySearch(int[] input, int value) {
+	public static int iterativeBinarySearch(int[] input, int value) 
+	{
 		int start = 0;
 		int end = input.length;
 
-		while (start < end) {
+		while (start < end) 
+		{
 			int midpoint = (start + end) / 2;
 			System.out.println("midpoint = " + midpoint);
 			if (input[midpoint] == value) {
 				return midpoint;
 			}
-			else if (input[midpoint] < value) {
+			else if (input[midpoint] < value) 
+			{
 				start = midpoint + 1;
 			}
 			else {
 				end = midpoint;
 			}
 		}
-
 		return -1;
 	}
 
-	public static int recursiveBinarySearch(int[] input, int value) {
+	public static int recursiveBinarySearch(int[] input, int value) 
+	{
 		return recursiveBinarySearch(input, 0, input.length, value);
 	}
 
-	public static int recursiveBinarySearch(int[] input, int start, int end, int value) {
-		if (start >= end) {
+	public static int recursiveBinarySearch(int[] input, int start, int end, int value) 
+	{
+		if (start >= end) 
+		{
 			return -1;
 		}
 
 		int midpoint = (start + end) / 2;
 		System.out.println("midpoint = " + midpoint);
 
-		if (input[midpoint] == value) {
+		if (input[midpoint] == value) 
+		{
 			return midpoint;
 		}
-		else if (input[midpoint] < value) {
+		else if (input[midpoint] < value) 
+		{
 			return recursiveBinarySearch(input, midpoint + 1, end, value);
 		}
-		else {
+		else 
+		{
 			return recursiveBinarySearch(input, start, midpoint, value);
 		}
 	}
