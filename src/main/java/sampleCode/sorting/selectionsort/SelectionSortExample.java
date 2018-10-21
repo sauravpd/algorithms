4b8d179c956bd09e3daad6ee3a2586dfd893eb29
saurav <sauravpd58@gmail.com>
2018-10-22 01:53:35 +0530

    commit changes in SelectionSortExample

--- src/main/java/sampleCode/sorting/selectionsort/SelectionSortExample.java
+++ src/main/java/sampleCode/sorting/selectionsort/SelectionSortExample.java
@@ -14,18 +14,21 @@ public class SelectionSortExample
 		{
 			int largest = 0;
 
-			for (int i = 1; i <= lastUnsortedIndex; i++) {
-				if (intArray[i] > intArray[largest]) {
+			for (int i = 1; i <= lastUnsortedIndex; i++) 
+			{
+				if (intArray[i] > intArray[largest]) 
+				{
 					largest = i;
 				}
 			}
 			swap(intArray, largest, lastUnsortedIndex);
 		}
-		for (int i = 0; i < intArray.length; i++) {
+		for (int i = 0; i < intArray.length; i++) 
+		{
 			System.out.println(intArray[i]);
 		}
 	}
-	
+
 	/**
 	 * swap
 	 * @param array
