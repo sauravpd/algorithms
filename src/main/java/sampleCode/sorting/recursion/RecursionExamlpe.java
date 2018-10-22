9282541e409fd3acf185e61ed6597a3cb2b5fd6b
saurav <sauravpd58@gmail.com>
2018-10-23 00:12:04 +0530

    added RecursionExamlpe class

---
+++ src/main/java/sampleCode/sorting/recursion/RecursionExamlpe.java
@@ -0,0 +1,33 @@
+package sampleCode.sorting.recursion;
+
+public class RecursionExamlpe 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(iterativeFactorial(4));
+		System.out.println(recursiveFactorial(4));
+	}
+	
+	public static int recursiveFactorial(int num) 
+	{
+		if (num == 0)
+		{
+			return 1;
+		}
+		return num * recursiveFactorial(num - 1);
+	}
+
+	public static int iterativeFactorial(int num) 
+	{
+		if (num == 0) 
+		{
+			return 1;
+		}
+		int factorial = 1;
+		for (int i = 1; i <= num; i++) 
+		{
+			factorial *= i;
+		}
+		return factorial;
+	}
+}
